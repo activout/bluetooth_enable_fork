636c995f96e7c41d74e3f62bf444f9f186bfaa4b
MrCsabaToth <csaba.toth.us@gmail.com>
2021-11-04 23:33:30 -0700

    Fixes #1 by swallowing the IllegalStateException and NPE + converting console log to Log

--- android/src/main/java/com/hui/bluetooth_enable/BluetoothEnablePlugin.java
+++ android/src/main/java/com/hui/bluetooth_enable/BluetoothEnablePlugin.java
@@ -67,7 +67,7 @@ public class BluetoothEnablePlugin implements FlutterPlugin, ActivityAware, Meth
             {  
                 Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
                 activity.startActivityForResult(enableBtIntent, REQUEST_ENABLE_BLUETOOTH);
-                System.out.println("rdddesult: " + result);
+                Log.d(TAG, "rdddesult: " + result);
                 pendingResult = result;
                 break;
             }
@@ -84,8 +84,8 @@ public class BluetoothEnablePlugin implements FlutterPlugin, ActivityAware, Meth
                 }
                 catch(InterruptedException e)
                 {
-                    System.out.println(e);
-                }             
+                    Log.e(TAG, "customEnable", e);
+                }
                 result.success("true");
                 break;
             }
@@ -99,17 +99,20 @@ public class BluetoothEnablePlugin implements FlutterPlugin, ActivityAware, Meth
 
     @Override
     public boolean onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_ENABLE_BLUETOOTH){        
-          if (resultCode == Activity.RESULT_OK) {
-            Log.d(TAG, "User enabled Bluetooth");
-            System.out.println("User enabled Bluetooth");
-            pendingResult.success("true");
-          }
-          else{
-              Log.d(TAG, "User did NOT enabled Bluetooth");
-              System.out.println("User did NOT enabled Bluetooth");
-              pendingResult.success("false");
-          }
+        if (requestCode == REQUEST_ENABLE_BLUETOOTH && pendingResult != null){
+            try {
+                if (resultCode == Activity.RESULT_OK) {
+                    Log.d(TAG, "User enabled Bluetooth");
+                    pendingResult.success("true");
+                } else {
+                    Log.d(TAG, "User did NOT enabled Bluetooth");
+                    pendingResult.success("false");
+                }
+            }
+            catch(IllegalStateException|NullPointerException e)
+            {
+                Log.d(TAG, "onActivityResult REQUEST_ENABLE_BLUETOOTH", e);
+            }
         }
         return false;
     }
@@ -124,16 +127,16 @@ public class BluetoothEnablePlugin implements FlutterPlugin, ActivityAware, Meth
                         BluetoothAdapter.ERROR);
                 switch (state) {
                     case BluetoothAdapter.STATE_OFF:
-                        System.out.println("STATE_OFF");
+                        Log.d(TAG, "BroadcastReceiver onReceive: STATE_OFF");
                         break;
                     case BluetoothAdapter.STATE_TURNING_OFF:
-                        System.out.println("STATE_TURNING_OFF");
+                        Log.d(TAG, "BroadcastReceiver onReceive: STATE_TURNING_OFF");
                         break;
                     case BluetoothAdapter.STATE_ON:
-                        System.out.println("STATE_ON");
+                        Log.d(TAG, "BroadcastReceiver onReceive: STATE_ON");
                         break;
                     case BluetoothAdapter.STATE_TURNING_ON:
-                        System.out.println("STATE_TURNING_ON");
+                        Log.d(TAG, "BroadcastReceiver onReceive: STATE_TURNING_ON");
                         break;
                 }
             }
@@ -143,7 +146,7 @@ public class BluetoothEnablePlugin implements FlutterPlugin, ActivityAware, Meth
     @Override
     public boolean onRequestPermissionsResult(
         int requestCode, String[] permissions, int[] grantResults) {
-        System.out.println("TWO");
+        Log.d(TAG, "onRequestPermissionsResult, TWO");
 
         return false;
     }
